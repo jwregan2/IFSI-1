542c5116cd3159e870c75a4ed1272dfecbcf3e0f
jwregan2 <Jack.Regan@ul.com>
2017-01-06 16:16:07 -0500

    Modified Script so that it can cycle through charts

--- Scripts/Plot_Mean.py
+++ Scripts/Plot_Mean.py
@@ -15,8 +15,14 @@ from datetime import timedelta
 data_location='../Data/'
 output_location='../Figures/'
 
-channel_left='Bedroom67ft'
-channel_right='Bedroom17ft'
+channels=pd.read_csv('../Info/Channels.csv')
+channels=channels.set_index('Chart')
+
+Exp_Des=pd.read_csv('../Info/Description_of_Experiments.csv')
+Exp_Des=Exp_Des.set_index('Experiment')
+
+# channel_left='Bedroom67ft'
+# channel_right='Bedroom17ft'
 N_rows=358
 N_columns=12
 
@@ -32,108 +38,117 @@ for i in range(len(colors)):
     r, g, b = colors[i]
     colors[i] = (r / 255., g / 255., b / 255.)
 
-Exp_Names=[]
-max_temp=0
-i=0
-temps=pd.DataFrame(np.zeros((N_rows,N_columns)))
-for f in os.listdir(data_location):
-	if f.endswith('.csv'):
-		data_file=pd.read_csv(data_location+f)
-		data_file=data_file.set_index('Time')
-
-		##NEED TO BE ABLE TO READ SIDE
-		Test_Name=f[:-4]
-		Exp_Names.append(Test_Name)
-		Exp_Num=int(Test_Name[11:])
-
-		## MUST LOAD IN EVENTS
-		print(Test_Name)
-		Events=pd.read_csv('../Info/Events/'+Test_Name+'_Events.csv')
 
 
-		Events=Events.set_index('Event')
-
-		if Exp_Num%2==0:
-			Side='Right'
-			channel=channel_right
-			Ignition = datetime.datetime.strptime(Events['Time']['Ignition BR1'], '%Y-%m-%d-%H:%M:%S')
-			Int_Time=Ignition+timedelta(seconds=359)
-			# Locations=['LRFront','LRRear','DRFront','DRRear','HallRight','Bedroom2','Bedroom1','HallRightHF']
-
-		elif Exp_Num%2==1:
-			Side='Left'
-			channel=channel_left
-			Ignition = datetime.datetime.strptime(Events['Time']['Ignition BR6'], '%Y-%m-%d-%H:%M:%S')
-			Int_Time=Ignition+timedelta(seconds=359)
-
-			# Locations=['LRFront','LRRear','DRFront','DRRear','HallLeft','Bedroom5','Bedroom6','HallLeftHF']
-		else:
-			 print ('ERROR 1')
-		temp_vec=data_file[channel][str(Ignition):str(Int_Time)]#Ig,Ig+358
-		temp_vec=temp_vec.reset_index()
-		max_temp=max([max_temp,max((temp_vec[channel]))])
 
 
-		for j in range(len(temps[i])):
-			if j<len(temp_vec):
-				temps.loc[j,i]=temp_vec[channel][j]
+
+
+for chart in channels.index.values:
+	print(chart)
+	Exp_Names=[]
+	max_temp=0
+	i=0
+	temps=pd.DataFrame(np.zeros((N_rows,N_columns)))
+	for f in os.listdir(data_location):
+		if f.endswith('.csv'):
+			data_file=pd.read_csv(data_location+f)
+			data_file=data_file.set_index('Time')
+
+			##NEED TO BE ABLE TO READ SIDE
+			Test_Name=f[:-4]
+			Exp_Names.append(Test_Name)
+			Exp_Num=int(Test_Name[11:])
+
+			## MUST LOAD IN EVENTS
+			print(Test_Name)
+			Events=pd.read_csv('../Info/Events/'+Test_Name+'_Events.csv')
+
+
+			Events=Events.set_index('Event')
+
+			if Exp_Num%2==0:
+				Side='Right'
+				channel=channels['Right_Channel'][chart]
+				Ignition = datetime.datetime.strptime(Events['Time']['Ignition BR1'], '%Y-%m-%d-%H:%M:%S')
+				Int_Time=Ignition+timedelta(seconds=359)
+				# Locations=['LRFront','LRRear','DRFront','DRRear','HallRight','Bedroom2','Bedroom1','HallRightHF']
+
+			elif Exp_Num%2==1:
+				Side='Left'
+				channel=channels['Left_Channel'][chart]
+				Ignition = datetime.datetime.strptime(Events['Time']['Ignition BR6'], '%Y-%m-%d-%H:%M:%S')
+				Int_Time=Ignition+timedelta(seconds=359)
+
+				# Locations=['LRFront','LRRear','DRFront','DRRear','HallLeft','Bedroom5','Bedroom6','HallLeftHF']
 			else:
-				continue
+				 print ('ERROR 1')
 
-		# print (temps)
-		i=i+1
-time_vector=np.linspace(1,358,358)
+			temp_vec=data_file[channel][str(Ignition):str(Int_Time)]#Ig,Ig+358
+			temp_vec=temp_vec.reset_index()
+			max_temp=max([max_temp,max((temp_vec[channel]))])
 
 
-##2 x standard DEV
-fig = figure()
-for i in range(12):
-	y = temps[i]
-	plot(time_vector[0:len(time_vector)-1],y[0:len(time_vector)-1],color=colors[i],marker=markers[i],markevery=50,ms=8,label=Exp_Names[i])
-plot(time_vector[0:len(time_vector)-1],temps[0:len(time_vector)-1].mean(axis=1),'k',label='average'+'AVG',linewidth=3)
-plt.fill_between(time_vector[0:len(time_vector)-1] ,temps[0:len(time_vector)-1].mean(axis=1)+2*temps[0:len(time_vector)-1].std(axis=1), temps[0:len(time_vector)-1].mean(axis=1)-2*temps[0:len(time_vector)-1].std(axis=1), facecolor='gray',alpha=0.5, interpolate=True,linewidth=3)
-# plt.fill_between(time_vector[0:len(time_vector)-1],.9*temps[0:len(time_vector)-1].mean(axis=1), 1.1*temps[0:len(time_vector)-1].mean(axis=1), facecolor='gray',alpha=0.5, interpolate=True,linewidth=3)
-ax1 = gca()
-xlabel('Time (s)', fontsize=20)
-ylabel('Temperature (C)', fontsize=20)
-xticks(fontsize=16)
-yticks(fontsize=16)
-legend(numpoints=1,loc=1,ncol=2,fontsize=16)
-axis([0, 1.1*N_rows, 0, 1.1*max_temp])
-box = ax1.get_position()
-ax1.set_position([box.x0, box.y0, box.width * 0.75, box.height])
-# ax1.set_xlim([0,1.1*N_rows])
+			for j in range(len(temps[i])):
+				if j<len(temp_vec):
+					temps.loc[j,i]=temp_vec[channel][j]
+				else:
+					continue
 
-# ax1.set_ylim([0,1.1*max_temp])
-ax1.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-grid(True)
+			# print (temps)
+			i=i+1
+	time_vector=np.linspace(1,358,358)
 
-savefig(output_location+ 'standard_dev/'+channel + '.pdf',format='pdf')
-close()
 
-##+- 10%
+	##2 x standard DEV
+	fig = figure()
+	for i in range(12):
+		y = temps[i]
+		plot(time_vector[0:len(time_vector)-1],y[0:len(time_vector)-1],color=colors[i],marker=markers[i],markevery=50,ms=8,label=Exp_Names[i])
+	plot(time_vector[0:len(time_vector)-1],temps[0:len(time_vector)-1].mean(axis=1),'k',label='average'+'AVG',linewidth=3)
+	plt.fill_between(time_vector[0:len(time_vector)-1] ,temps[0:len(time_vector)-1].mean(axis=1)+2*temps[0:len(time_vector)-1].std(axis=1), temps[0:len(time_vector)-1].mean(axis=1)-2*temps[0:len(time_vector)-1].std(axis=1), facecolor='gray',alpha=0.5, interpolate=True,linewidth=3)
+	# plt.fill_between(time_vector[0:len(time_vector)-1],.9*temps[0:len(time_vector)-1].mean(axis=1), 1.1*temps[0:len(time_vector)-1].mean(axis=1), facecolor='gray',alpha=0.5, interpolate=True,linewidth=3)
+	ax1 = gca()
+	xlabel('Time (s)', fontsize=20)
+	ylabel('Temperature (C)', fontsize=20)
+	xticks(fontsize=16)
+	yticks(fontsize=16)
+	legend(numpoints=1,loc=1,ncol=2,fontsize=16)
+	axis([0, 1.1*N_rows, 0, 1.1*max_temp])
+	box = ax1.get_position()
+	ax1.set_position([box.x0, box.y0, box.width * 0.75, box.height])
+	# ax1.set_xlim([0,1.1*N_rows])
 
-fig = figure()
-for i in range(12):
-	y = temps[i]
-	plot(time_vector[0:len(time_vector)-1],y[0:len(time_vector)-1],color=colors[i],marker=markers[i],markevery=50,ms=8,label=Exp_Names[i].replace('_',' '))
-plot(time_vector[0:len(time_vector)-1],temps[0:len(time_vector)-1].mean(axis=1),'k',label='Average',linewidth=3)
-# plt.fill_between(time_vector[0:len(time_vector)-1] ,temps[0:len(time_vector)-1].mean(axis=1)+2*temps[0:len(time_vector)-1].std(axis=1), temps[0:len(time_vector)-1].mean(axis=1)-2*temps[0:len(time_vector)-1].std(axis=1), facecolor='gray',alpha=0.5, interpolate=True,linewidth=3)
-plt.fill_between(time_vector[0:len(time_vector)-1],.9*temps[0:len(time_vector)-1].mean(axis=1), 1.1*temps[0:len(time_vector)-1].mean(axis=1), facecolor='gray',alpha=0.5, interpolate=True,linewidth=3)
-ax1 = gca()
-xlabel('Time (s)', fontsize=20)
-ylabel('Temperature (C)', fontsize=20)
-xticks(fontsize=16)
-yticks(fontsize=16)
-legend(numpoints=1,loc=1,ncol=2,fontsize=16)
-axis([0, 1.1*N_rows, 0, 1.1*max_temp])
-box = ax1.get_position()
-ax1.set_position([box.x0, box.y0, box.width * 0.75, box.height])
-# ax1.set_xlim([0,1.1*N_rows])
+	# ax1.set_ylim([0,1.1*max_temp])
+	ax1.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+	grid(True)
 
-# ax1.set_ylim([0,1.1*max_temp])
-ax1.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-grid(True)
+	savefig(output_location+ 'standard_dev/'+chart + '.pdf',format='pdf')
+	close()
 
-savefig(output_location+ '10percent_TC/'+ channel + '.pdf',format='pdf')
-close()
+	##+- 10%
+
+	fig = figure()
+	for i in range(12):
+		y = temps[i]
+		plot(time_vector[0:len(time_vector)-1],y[0:len(time_vector)-1],color=colors[i],marker=markers[i],markevery=50,ms=8,label=Exp_Names[i].replace('_',' '))
+	plot(time_vector[0:len(time_vector)-1],temps[0:len(time_vector)-1].mean(axis=1),'k',label='Average',linewidth=3)
+	# plt.fill_between(time_vector[0:len(time_vector)-1] ,temps[0:len(time_vector)-1].mean(axis=1)+2*temps[0:len(time_vector)-1].std(axis=1), temps[0:len(time_vector)-1].mean(axis=1)-2*temps[0:len(time_vector)-1].std(axis=1), facecolor='gray',alpha=0.5, interpolate=True,linewidth=3)
+	plt.fill_between(time_vector[0:len(time_vector)-1],.9*temps[0:len(time_vector)-1].mean(axis=1), 1.1*temps[0:len(time_vector)-1].mean(axis=1), facecolor='gray',alpha=0.5, interpolate=True,linewidth=3)
+	ax1 = gca()
+	xlabel('Time (s)', fontsize=20)
+	ylabel('Temperature (C)', fontsize=20)
+	xticks(fontsize=16)
+	yticks(fontsize=16)
+	legend(numpoints=1,loc=1,ncol=2,fontsize=16)
+	axis([0, 1.1*N_rows, 0, 1.1*max_temp])
+	box = ax1.get_position()
+	ax1.set_position([box.x0, box.y0, box.width * 0.75, box.height])
+	# ax1.set_xlim([0,1.1*N_rows])
+
+	# ax1.set_ylim([0,1.1*max_temp])
+	ax1.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+	grid(True)
+
+	savefig(output_location+ '10percent_TC/'+ chart + '.pdf',format='pdf')
+	close()
